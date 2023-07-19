a51d4794c66f95ea9c7dba3e79608eb81a2bbea0
ZeynepDuzen <zeynepduzen59@gmail.com>
2023-07-19 05:57:49 +0300

    Proje dosyası ekle.

---
+++ StudyingMethods2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyingMethods2
+{
+    internal class Program
+    {
+        private static void Univ()
+        {
+            Console.WriteLine("Department 1 : Literature");
+            Console.WriteLine("Department 2 : Psychology");
+            Console.WriteLine("Department 3 : Maths");
+        }
+        static void Main(string[] args)
+        {
+            Univ();
+            Console.Read();
+        }
+    }
+}
